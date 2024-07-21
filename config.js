ed237823b62bae93a979cbba6caabbc983ae77a5
sanchitha23 <167330247+sanchitha23@users.noreply.github.com>
2024-07-21 21:31:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://gi
 global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴳᴴᴼˢᵀ ⱽ1 ᴹᴰ" 
 
 
-global.devs = "923184474176" // Developer Contact
+global.devs = "94741140620" // Developer Contact
 global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "94741140620";
 
